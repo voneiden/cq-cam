da8bfdff50e6ce9ac5c3b08d6da9d8022e96be0a
Matti Eiden <snaipperi@gmail.com>
2022-03-18 23:56:28 +0200

    Drill: poc

--- cq_cam/operations/drill.py
+++ cq_cam/operations/drill.py
@@ -1,8 +1,14 @@
 from dataclasses import dataclass, field
 from typing import Optional, List, Union
 
+from cq_cam.commands.command import Rapid, Plunge
 from cq_cam.operations.base_operation import Task, OperationError
 import cadquery as cq
+
+from cq_cam.operations.strategy import Strategy
+from cq_cam.utils.utils import flatten_list
+
+_op_o_shapes = Union[cq.Wire, cq.Face, cq.Vector]
 
 
 @dataclass
@@ -11,52 +17,85 @@ class Drill(Task):
     """ The cadquery Workplane containing faces and/or
     wires that the profile will operate on. 
     """
-    points: Optional[List[Union[cq.Location, cq.Vector]]] = None
-    wires: Optional[List[cq.Wire]] = None
-    faces: Optional[List[cq.Face]] = None
+    o: Union[cq.Workplane, List[_op_o_shapes], _op_o_shapes] = None
     depth: Optional[float] = None
 
-    def __post__init__(self):
+    def __post_init__(self):
         # TODO max depth
         # TODO evacuate chips?
         transform_f = self.job.workplane.plane.toWorldCoords
-        drill_points: List[Union[cq.Location, cq.Vector]] = []
+        drill_vectors: List[cq.Vector] = []
 
-        if self.wp is None:
-            raise OperationError("wp must be defined")
+        if self.o is None:
+            raise OperationError("o must be defined")
 
-        for obj in self.wp.objects:
-            if isinstance(obj, cq.Location):
-                drill_points.append(obj)
-            elif isinstance(obj, cq.Vector):
-                drill_points.append(transform_f(obj))
+        for obj in self._o_objects(self.o):
+            if isinstance(obj, cq.Vector):
+                drill_vectors.append(transform_f(obj))
             elif isinstance(obj, cq.Wire):
-                drill_points.append(transform_f(cq.Face.makeFromWires(obj).Center()))
+                drill_vectors.append(transform_f(cq.Face.makeFromWires(obj).Center()))
             elif isinstance(obj, cq.Face):
                 if obj.innerWires():
                     for wire in obj.innerWires():
-                        drill_points.append(transform_f(cq.Face.makeFromWires(wire).Center()))
+                        drill_vectors.append(transform_f(cq.Face.makeFromWires(wire).Center()))
                 else:
-                    drill_points.append(transform_f(cq.Face.makeFromWires(obj.outerWire()).Center()))
+                    drill_vectors.append(transform_f(cq.Face.makeFromWires(obj.outerWire()).Center()))
             else:
                 raise OperationError(f'Object type "{type(obj)}" not supported by Profile operation')
 
-        if not drill_points:
+        if not drill_vectors:
             raise OperationError("Given wp does not contain anything to do")
-
-        for point in self.points:
-            drill_points.append(
-                point if isinstance(point, cq.Location) else transform_f(point)
-            )
-
-        for wire in self.wires:
-            drill_points.append(transform_f(cq.Face.makeFromWires(wire).Center()))
-
-        for face in self.faces:
-            for wire in face.innerWires():
-                drill_points.append(transform_f(cq.Face.makeFromWires(wire).Center()))
 
         # TODO generate commands
         # TODO optimize order
-if __name__ == '__main__':
-    d = Drill(None, 0, [], )
+
+        drill_points = [(point.x, point.y) for point in drill_vectors]
+        ordered_drill_points = []
+        cut_sequences = []
+        last = None
+        while drill_points:
+            drill_point = Strategy._pick_nearest(last, drill_points) if last else drill_points[0]
+            ordered_drill_points.append(drill_point)
+            drill_points.pop(drill_points.index(drill_point))
+            last = drill_point
+
+
+        for point in ordered_drill_points:
+            cut_sequences.append([
+                Rapid(x=None, y=None, z=self.clearance_height),
+                Rapid(x=point[0], y=point[1], z=None),
+                Rapid(x=None, y=None, z=self.top_height),
+                Plunge(z=-10),  # TODO depth
+                Rapid(x=None, y=None, z=self.clearance_height),
+            ])
+        cut_sequences = flatten_list(cut_sequences)
+        self.commands = cut_sequences
+
+
+def demo():
+    from cq_cam.job import Job
+    from cq_cam.commands.base_command import Unit
+    from cq_cam.visualize import visualize_task
+
+    result = cq.Workplane("front").box(20.0, 20.0, 2).faces('>Z').workplane().pushPoints([
+        (3, 3),
+        (-5, -8),
+        (0,0),
+        (5,2),
+        (7,-3),
+        (-8,2)
+    ]).circle(
+        1).cutThruAll()
+    # show_object(result.faces('<Z[1]'))
+    job_plane = result.faces('>Z').workplane()
+    job = Job(job_plane, 300, 100, Unit.METRIC, 5)
+    op = Drill(job=job, clearance_height=5, top_height=0, o=result.faces('>Z').objects[0].innerWires())
+    toolpath = visualize_task(job, op, as_edges=False)
+    # result.objects += toolpath
+    show_object(result)
+    show_object(toolpath)
+    show_object(result.faces('>Z'), 'avoid', {'color': 'red'})
+
+
+if 'show_object' in locals() or __name__ == '__main__':
+    demo()
