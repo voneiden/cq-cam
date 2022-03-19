611842655d79e7e185850afc20e28915ff8ad0fd
Matti Eiden <snaipperi@gmail.com>
2022-03-20 00:15:51 +0200

    Update env/requirements/setup (#7)

--- setup.py
+++ setup.py
@@ -3,6 +3,11 @@
 from setuptools import setup
 
 setup(
-    setup_requires=['pbr'],
-    pbr=True,
+    name='cq-cam',
+    version='0.1',
+    description='Cadquery CAM',
+    author='Matti Eiden',
+    author_email='snaipperi@gmail.com',
+    packages=['cq_cam'],  #same as name
+    install_requires=['pyclipper', 'numpy'],
 )
