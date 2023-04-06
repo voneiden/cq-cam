2e1476c8e3cf6cf2ae1dab17a8aac53365796cfc
Matti Eiden <snaipperi@gmail.com>
2023-04-06 23:04:11 +0300

    Add offset_face to offset utils and implement tests

--- src/cq_cam/utils/offset.py
+++ src/cq_cam/utils/offset.py
@@ -1,4 +1,6 @@
-from typing import Optional, Tuple, TypeAlias, Union
+from typing import List, Optional, Tuple, TypeAlias, Union
+
+import cadquery as cq
 
 OffsetToolRadiusMultiplier: TypeAlias = float
 OffsetDistance: TypeAlias = float
@@ -17,3 +19,16 @@ def calculate_offset(tool_radius: float, offset: Optional[OffsetInput], default=
 
     except TypeError:
         return tool_radius * offset
+
+
+def offset_face(
+    face: cq.Face, outer_offset: float, inner_offset: float
+) -> List[cq.Face]:
+    offset_faces = []
+    op_outers = face.outerWire().offset2D(outer_offset)
+    op_inners = []
+    for inner in face.innerWires():
+        op_inners += inner.offset2D(inner_offset)
+    for op_outer in op_outers:
+        offset_faces.append(cq.Face.makeFromWires(op_outer, op_inners))
+    return offset_faces
--- src/cq_cam/utils/tests/test_offset.py
+++ src/cq_cam/utils/tests/test_offset.py
@@ -1,4 +1,6 @@
-from cq_cam.utils.offset import calculate_offset
+import cadquery as cq
+
+from cq_cam.utils.offset import calculate_offset, offset_face
 
 
 def test_calculate_offset():
@@ -8,3 +10,42 @@ def test_calculate_offset():
     assert calculate_offset(1, -1) == -1
     assert calculate_offset(1, (-1, 0.5)) == -0.5
     assert calculate_offset(1, (0, 1.05)) == 1.05
+
+
+def test_offset_face():
+    rect1 = cq.Workplane().rect(10, 10).objects[0]
+    rect2 = (
+        cq.Workplane()
+        .rect(
+            5,
+            5,
+        )
+        .objects[0]
+    )
+    # noinspection PyTypeChecker
+    face = cq.Face.makeFromWires(rect1, [rect2])
+    new_faces = offset_face(face, outer_offset=-1, inner_offset=1)
+    assert len(new_faces) == 1
+    new_face = new_faces[0]
+
+    outer_points = set([vx.toTuple() for vx in new_face.outerWire().Vertices()])
+
+    assert len(new_face.innerWires()) == 1
+    inner_points = set([vx.toTuple() for vx in new_face.innerWires()[0].Vertices()])
+
+    assert {
+        (4.0, 4.0, 0.0),
+        (-4.0, -4.0, 0.0),
+        (4.0, -4.0, 0.0),
+        (-4.0, 4.0, 0.0),
+    } == outer_points
+    assert {
+        (-3.5, -2.5, 0.0),
+        (-3.5, 2.5, 0.0),
+        (-2.5, -3.5, 0.0),
+        (-2.5, 3.5, 0.0),
+        (2.5, -3.5, 0.0),
+        (2.5, 3.5, 0.0),
+        (3.5, -2.5, 0.0),
+        (3.5, 2.5, 0.0),
+    } == inner_points
