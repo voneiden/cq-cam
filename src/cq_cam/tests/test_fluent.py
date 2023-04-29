a5c2d24988750c911f6a609afa4e4a227ef053d5
Matti Eiden <snaipperi@gmail.com>
2023-04-30 00:33:15 +0300

    Kill code smells

--- src/cq_cam/tests/test_fluent.py
+++ src/cq_cam/tests/test_fluent.py
@@ -12,16 +12,21 @@ def to_occ_color():
     _cq_utils.to_occ_color = lambda x: Quantity_Color(0, 0, 0, Quantity_TOC_RGB)
 
 
+@pytest.fixture
+def show_object():
+    def show_object(*args, **kwargs):
+        return None
+
+    return show_object
+
+
 def test_fluent_show__no_show_object(job, top_face):
     job = job.profile(top_face)
     with pytest.raises(ValueError):
         job.show()
 
 
-def test_fluent_show__cq_editor(job, top_face, caplog):
-    def show_object(*args, **kwargs):
-        pass
-
+def test_fluent_show__cq_editor(job, top_face, caplog, show_object):
     show_object.__module__ = "cq_editor.test"
 
     job = job.profile(top_face)
@@ -29,10 +34,7 @@ def test_fluent_show__cq_editor(job, top_face, caplog):
     assert "Unsupported show_object source module" not in caplog.text
 
 
-def test_fluent_show__ocp_vscode(job, top_face, caplog):
-    def show_object(*args, **kwargs):
-        pass
-
+def test_fluent_show__ocp_vscode(job, top_face, caplog, show_object):
     show_object.__module__ = "ocp_vscode.test"
 
     job = job.profile(top_face)
@@ -40,10 +42,7 @@ def test_fluent_show__ocp_vscode(job, top_face, caplog):
     assert "Unsupported show_object source module" not in caplog.text
 
 
-def test_fluent_show__unknown(job, top_face, caplog):
-    def show_object(*args, **kwargs):
-        pass
-
+def test_fluent_show__unknown(job, top_face, caplog, show_object):
     show_object.__module__ = "unknown.test"
 
     job = job.profile(top_face)
@@ -52,10 +51,7 @@ def test_fluent_show__unknown(job, top_face, caplog):
     assert "Unsupported show_object source module (unknown)" in caplog.text
 
 
-def test_fluent_show__autodiscovery(job, top_face, caplog):
-    def show_object(*args, **kwargs):
-        pass
-
+def test_fluent_show__autodiscovery(job, top_face, caplog, show_object):
     show_object.__module__ = "cq_editor.test"
     import __main__
 
