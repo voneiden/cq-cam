5f2688b428ce736e1aef639547179e29f6ebdb0b
Matti Eiden <snaipperi@gmail.com>
2023-04-30 00:27:06 +0300

    Mock to_occ_color as it is not available in the github action

--- src/cq_cam/tests/test_fluent.py
+++ src/cq_cam/tests/test_fluent.py
@@ -1,4 +1,15 @@
+import sys
+from unittest.mock import Mock
+
 import pytest
+from OCP.Quantity import Quantity_Color, Quantity_TOC_RGB
+
+
+@pytest.fixture(autouse=True)
+def to_occ_color():
+    _cq_utils = Mock()
+    sys.modules["cq_editor.cq_utils"] = _cq_utils
+    _cq_utils.to_occ_color = lambda x: Quantity_Color(0, 0, 0, Quantity_TOC_RGB)
 
 
 def test_fluent_show__no_show_object(job, top_face):
